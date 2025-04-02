64a216b222ea8867698c8964d42d158ba1125b3a
Filipe Magal <Filipi99ff@gmail.com>
2025-04-02 14:30:19 -0300

    Initial commit

---
+++ src/test/java/com/insexba/relacionamento_insex/RelacionamentoInsexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.insexba.relacionamento_insex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RelacionamentoInsexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
